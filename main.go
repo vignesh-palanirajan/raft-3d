25d20569e65dcb4b3fd1e021a207e9b8dcd6a719
vandanaj0110 <vandanaj0110@gmail.com>
2025-04-11 12:56:44 +0530

    Updated main.go

--- main.go
+++ main.go
@@ -6,14 +6,24 @@ import (
 	"net/http"
 )
 
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Raft3D API is running!")
-}
+var raftNode *Raft // Global Raft node
 
 func main() {
+	// Initialize Raft node
+	var err error
+	raftNode, err = NewRaftNode("node1", "./data")
+	if err != nil {
+		log.Fatalf("Error starting Raft: %v", err)
+	}
+
+	// Start HTTP server
 	http.HandleFunc("/health", healthCheckHandler)
 
 	port := "8080"
 	fmt.Println("Server running on port", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Raft3D API is running!")
 }
