c5fe69b8b0594ae928eed41a34a48585cf23a103
Vignesh Palanirajan <113622877+vignesh-palanirajan@users.noreply.github.com>
2025-04-19 13:11:31 +0530

    Update main.go
    Adding multiple nodes

--- main.go
+++ main.go
@@ -1,29 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"fmt"
 )
 
-var raftNode *Raft // Global Raft node
-
 func main() {
-	// Initialize Raft node
-	var err error
-	raftNode, err = NewRaftNode("node1", "./data")
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: go run main.go <nodeID>")
+	}
+
+	nodeID := os.Args[1]
+
+	var bindAddr, raftAddr, httpPort string
+
+	if nodeID == "node1" {
+		bindAddr = "127.0.0.1:5000"
+		raftAddr = "127.0.0.1:5000"
+		httpPort = "8080"
+	} else if nodeID == "node2" {
+		bindAddr = "127.0.0.1:5001"
+		raftAddr = "127.0.0.1:5001"
+		httpPort = "8081"
+	} else {
+		log.Fatalf("Unknown node ID: %s", nodeID)
+	}
+
+	err := NewRaftNode(nodeID, bindAddr, raftAddr, httpPort)
 	if err != nil {
 		log.Fatalf("Error starting Raft: %v", err)
 	}
 
-	// Start HTTP server
-	http.HandleFunc("/health", healthCheckHandler)
+	// Follower joins the cluster
+	if nodeID != "node1" {
+		joinURL := fmt.Sprintf("http://127.0.0.1:8080/join?addr=%s&id=%s", raftAddr, nodeID)
+		resp, err := http.Get(joinURL)
+		if err != nil || resp.StatusCode != http.StatusOK {
+			log.Fatalf("Failed to join cluster: %v", err)
+		}
+	}
 
-	port := "8080"
-	fmt.Println("Server running on port", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-}
-
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Raft3D API is running!")
+	select {} // block forever
 }
